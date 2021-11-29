7f57b080b0fed870fd5b9e53d180a377e086d45d
Frederico Bublitz <fred@Fredericos-MacBook-Pro.local>
2021-11-29 17:28:19 -0300

    Solução

---
+++ URI1005.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    
+
+float media, A, B;
+ 
+scanf("%f", &A);
+scanf("%f", &B); 
+
+media = ((A * 3.5) + (B * 7.5)) / 11;
+ 
+printf("MEDIA = %.5f\n", media); 
+ 
+return 0;
+}

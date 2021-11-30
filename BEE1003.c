a289784547ee191ff9edb519e7452271b04d76ef
Frederico Bublitz <fred@Fredericos-MacBook-Pro.local>
2021-11-30 14:31:19 -0300

    Solução

---
+++ BEE1003.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    int A, B;    
+    scanf("%d%d", &A, &B);
+    printf("SOMA = %d\n", A + B);
+    return 0;
+}
